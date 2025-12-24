f6d46d139542457a45f50b30d326b2d3dc4281a2
chihpao <chihpaoo@gmail.com>
2025-12-24 13:42:42 +0800

    改localhost啟動port號

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  devServer: {
+    port: 4211,
+  },
+
   css: ['~/assets/css/main.css'],
 
   postcss: {
